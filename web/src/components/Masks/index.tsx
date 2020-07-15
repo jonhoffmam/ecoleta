dde2daa6c39808f24d78d86d4507bdc44ddf4cee
jonhoffmam <jon.hoffmam@yahoo.com.br>
2020-07-14 23:59:48 -0300

    Create masks for zip code

---
+++ web/src/components/Masks/index.tsx
@@ -0,0 +1,17 @@
+const cpfMask = (value: string) => {
+  return value
+    .replace(/\D/g, "")
+    .replace(/(\d{3})(\d)/, "$1.$2")
+    .replace(/(\d{3})(\d)/, "$1.$2")
+    .replace(/(\d{3})(\d{1,2})/, "$1-$2")
+    .replace(/(-\d{2})\d+?$/, "$1");
+};
+
+const zipCodeMask = (value: string) => {
+  return value
+    .replace(/\D/g, "")
+    .replace(/(\d{5})(\d)/, "$1-$2")
+    .replace(/(-\d{3})\d+?$/, "$1");
+};
+
+export { cpfMask, zipCodeMask };
