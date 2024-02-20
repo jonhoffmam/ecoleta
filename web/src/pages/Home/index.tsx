c81718e1b59a13247a1b86bc93b52483c7f5903d
Jonathan Hoffmam Pivetta <jon.hoffmam@gmail.com>
2024-02-20 13:05:02 -0300

    Corrects routes and styles

--- web/src/pages/Home/index.tsx
+++ web/src/pages/Home/index.tsx
@@ -1,49 +1,45 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
 import { FiLogIn, FiSearch } from 'react-icons/fi';
+import { Link } from 'react-router-dom';
 import Search from '../../components/Search';
 
-import './styles.css';
 import logo from '../../assets/logo.svg';
-
-
+import './styles.css';
 
 const Home = () => {
-	const [openSearch, setOpenSearch] = useState(false);
+  const [openSearch, setOpenSearch] = useState(false);
 
-	return (
-		<div id="page-home">
-			<div className="content">
-				<header>
-					<img src={logo} alt="Ecoleta"/>
-				</header>
+  return (
+    <div id='page-home'>
+      <div className='content'>
+        <header>
+          <img src={logo} alt='Ecoleta' />
+        </header>
 
-				<main>
-					<h1>Seu marketplace de coleta de resíduos.</h1>
-					<p>Ajudamos pessoas a encontrarem pontos de coleta de forma eficiente.</p>
+        <main>
+          <h1>Seu marketplace de coleta de resíduos.</h1>
+          <p>Ajudamos pessoas a encontrarem pontos de coleta de forma eficiente.</p>
 
-					<Link to="/create-point">
-						<span>
-							<FiLogIn />
-						</span>
-						<strong>Cadastrar ponto de coleta</strong>
-					</Link>
+          <Link to='/create-point'>
+            <span>
+              <FiLogIn />
+            </span>
+            <strong>Cadastrar ponto de coleta</strong>
+          </Link>
 
-					<button className="buttonOpenSearch" onClick={() => setOpenSearch(!openSearch)}>
-						<span>							
-							<FiSearch />							
-						</span>
-						<strong>Pesquisar ponto de coleta</strong>
-					</button>
+          <button className='buttonOpenSearch' onClick={() => setOpenSearch(!openSearch)}>
+            <span>
+              <FiSearch />
+            </span>
+            <strong>Pesquisar ponto de coleta</strong>
+          </button>
 
-					
-					<Search value={openSearch} onClick={() => setOpenSearch(!openSearch)} />
-								
-					
-				</main>
-			</div>
-		</div>
-	);
-}
+          <Search value={openSearch} onClick={() => setOpenSearch(!openSearch)} />
+        </main>
+        <div className='img-background'></div>
+      </div>
+    </div>
+  );
+};
 
 export default Home;
--- web/src/routes.tsx
+++ web/src/routes.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Routes as RoutesContainer } from 'react-router-dom';
 
 import CreatePoint from './pages/CreatePoint';
 import Home from './pages/Home';
@@ -8,9 +8,11 @@ import SearchPoint from './pages/SearchPoint';
 const Routes = () => {
   return (
     <BrowserRouter>
-      <Route Component={Home} path='/' />
-      <Route Component={CreatePoint} path='/create-point' />
-      <Route Component={SearchPoint} path='/search-point/:typeLocal/:setLocal' />
+      <RoutesContainer>
+        <Route Component={Home} path='/' />
+        <Route Component={CreatePoint} path='/create-point' />
+        <Route Component={SearchPoint} path='/search-point/:typeLocal/:setLocal' />
+      </RoutesContainer>
     </BrowserRouter>
   );
 };
